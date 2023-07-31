058ecaf3e3863f2c0a47156971c09d1138dcff28
khaled agrama <khaled@users.noreply.github.com>
2023-07-30 21:04:39 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.go = nixpkgs.legacyPackages.x86_64-linux.go;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.go;
 
   };
 }
