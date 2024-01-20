94284b7738dedf4711ac625387fbedaa230ff792
khaled agrama <khaled@users.noreply.github.com>
2024-01-20 12:20:04 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.delve = nixpkgs.legacyPackages.x86_64-linux.delve;
+    packages.x86_64-linux.main = nixpkgs.legacyPackages.x86_64-linux.nodejs;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.delve;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.main;
 
   };
 }
