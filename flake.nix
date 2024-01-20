ac958bd7932e4935debca49407dacd14c495ac44
khaled agrama <khaled@users.noreply.github.com>
2024-01-20 12:12:35 -0500

    fix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.go = nixpkgs.legacyPackages.x86_64-linux.go;
+    packages.x86_64-linux.delve = nixpkgs.legacyPackages.x86_64-linux.delve;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.delve;
 
