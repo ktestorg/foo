85602867c83c022d24b465b6d2051a2078bf8bcd
khaled agrama <khaled@users.noreply.github.com>
2024-01-20 12:04:37 -0500

    delve as default

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
     packages.x86_64-linux.go = nixpkgs.legacyPackages.x86_64-linux.go;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.go;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.delve;
 
   };
 }
